78deef6027ebf4be5108d2128508a9fcac8001b4
cvols <chris.volonnino@gmail.com>
2017-12-05 21:38:21 -0500

    updates

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,6 @@
 var mysql = require('mysql')
 
-var connection = mysql.createConnection({
-    port: 3306,
-    host: 'localhost',
-    user: 'chris',
-    password: 'password',
-    database: 'burgers_db'
-})
+var connection;
 
 if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL)
